124dfdb0eb5afa4b56b4c9edcf784b230091c9c9
Francisco <piolifl@gmail.com>
2021-12-26 10:55:00 -0300

    Se modifica horario de cierre

--- Ratios_main_V10.py
+++ Ratios_main_V10.py
@@ -33,33 +33,39 @@ call = {
         '3': ['0', 1,  21,'0',  5,  13.95],
         '4': ['250', 2,  4.5,'270',  3,  2.55]
         }
+
 while True:
+    if time.strftime("%H:%M:%S") >= '17:00:10':
+        print('...................... MERCADO CERRADO 17HS .......................')
+        break
     ggal = ticker['acciones']['48']['ggal'].precio_LA()
     for i, e in call.items():
         while e[0] != '0':
-            ratioActual = round(strikeCall(e[0]).precio_BI()  / strikeCall(e[3]).precio_OF() ,2)
+            while True:
+                if strikeCall(e[0]).precio_BI() != 1000 != strikeCall(e[3]).precio_OF():
+                    if strikeCall(e[0]).cantidad_BI() >= e[1] and strikeCall(e[0]).cantidad_OF() >= e[4]:
+                        break
+                    else:
+                        print('Esperando compradores/vendedores ',time.strftime("%H:%M:%S")),time.sleep(1)
+                else:
+                    print('Esperando completar los precios ',time.strftime("%H:%M:%S")),time.sleep(1)
+
+            ratioActual = round(strikeCall(e[0]).precio_BI()  / strikeCall(e[3]).precio_OF(),2)
             ratioEntrada = round(e[2]  / e[5] ,2)
             saldo = round(
                 ((strikeCall(e[0]).precio_BI() * e[1] * 100 * (1 - costos)) - (e[2] * e[1] * 100) * (1 + costos)) -
                 ((strikeCall(e[3]).precio_OF() * e[4] * 100 * (1 + costos)) - (e[5] * e[4] * 100)) * (1 - costos),2)
             
-            if ratioActual > ratioEntrada * (1 + 0.1) and saldo > 10 and strikeCall(e[0]).precio_BI() != 1000 != strikeCall(e[3]).precio_OF():
+            if ratioActual > ratioEntrada * (1 + 0.1) and saldo > 100:
 
-                if strikeCall(e[0]).cantidad_BI() >= e[1] and strikeCall(e[0]).cantidad_OF() >= e[4]:
+                print(f'CERRADO bull CALL: {e[0]} = {strikeCall(e[0]).precio_BI()}  // {e[3]} = {strikeCall(e[3]).precio_OF()} RESULT = {saldo} ')
 
-                    print(f'CERRADO bull CALL: {e[0]} = {strikeCall(e[0]).precio_BI()}  // {e[3]} = {strikeCall(e[3]).precio_OF()} RESULT = {saldo} ')
+                vender(strikeCall(e[0]),e[1],strikeCall(e[0]).precio_BI())
+                comprar(strikeCall(e[3]),e[4],strikeCall(e[3]).precio_OF())
 
-                    vender(strikeCall(e[0]),e[1],strikeCall(e[0]).precio_BI())
-                    comprar(strikeCall(e[3]),e[4],strikeCall(e[3]).precio_OF())
+                e[0] = '0'
 
-                    e[0] = '0'
-                else:
-                    print('Ratio positivo , pero no hay suficientes compradores/vendedores ',time.strftime("%H:%M:%S")),time.sleep(2)
-                    break
             else:
                 print(f'GGAL: {ggal} - bull CALL: {e[0]} = {strikeCall(e[0]).precio_BI()}  // {e[3]} = {strikeCall(e[3]).precio_OF()} da ratioEntrada:{ratioEntrada} /// ratioActual {ratioActual} = {saldo} /// ',time.strftime("%H:%M:%S")),time.sleep(1)
                 break
-    if time.strftime("%H:%M:%S") >= '17:00:10':
-        print('...................... MERCADO CERRADO 17HS .......................')
-        break
        
