5d536c27816537c1bee5e5213ea2e6cded56ac6a
Francisco <piolifl@gmail.com>
2022-01-11 13:40:53 -0300

    Ajunte en limites de cedears

--- Bonos_main_V3.py
+++ Bonos_main_V3.py
@@ -55,9 +55,8 @@ def cclCI(bono):
 
 
 def cruce(t1,t2,t3,t4):
-    global vendoA,comproB,vendoB,comproA,saldoA,saldoB, cant
-
-    if t1 == 'aapl' or t1 == 'ko':
+    global vendoA,comproB,vendoB,comproA,saldoA,saldoB
+    if name1 == 'aapl' or name1 == 'ko':
         vendoA = round(((t1) * (1 - costos)) * cant,2)
         comproB = round(vendoA / ((t2/100) * (1 + costos)),0)
         vendoB = round(comproB * ((t3/100) * (1 - costos)),2)
@@ -68,9 +67,9 @@ def cruce(t1,t2,t3,t4):
         print(f'B:{comproB}',end=" ")
         print(f'$:{vendoB}',end=" ")
         print(f'FIN:{comproA}',end=" ")
-    elif t2 == 'aapl' or t2 == 'ko':
+    if name2 == 'aapl' or name2 == 'ko':
         vendoA = round(((t1/100) * (1 - costos)) * cant,2)
-        comproB = round(vendoA / ((t2) * (1 + costos)),0)
+        comproB = round(vendoA / ((t2/100) * (1 + costos)),0)
         vendoB = round(comproB * ((t3) * (1 - costos)),2)
         comproA = round(vendoB / ((t4/100) * (1 + costos)),0)
         saldoA = round(vendoA - comproB * (t2),2)
@@ -91,10 +90,13 @@ def cruce(t1,t2,t3,t4):
         print(f'$:{vendoB}',end=" ")
         print(f'FIN:{comproA}',end=" ") 
 
+
 def vuelta(a,b,c,d):
-    global limite,gana,moneda1,moneda2,moneda3
+    global limite,ganaAL30,moneda1,moneda2,moneda3,name1,name2,cant
     for i,e in ins.items():
         cant = e[1]
+        name1 = e[0]
+        name2 = e[2]
         while e[0] != '0':
             t1 = a(e[0]).precio_BI()
             t2 = b(e[2]).precio_OF()
@@ -103,7 +105,8 @@ def vuelta(a,b,c,d):
             cruce(t1,t2,t3,t4)
             if limite > 0:
                 if t1 != 1000 and t2 != 1000 and t3 != 1000 and t4 != 1000:
-                    if comproA > e[1] :
+
+                    if comproA > e[1] +1 :
                         if a(e[0]).cantidad_BI() > e[1] and b(e[2]).cantidad_OF() >= comproB and c(e[2]).cantidad_BI() >= comproB and d(e[0]).cantidad_OF() >= comproA:
 
                             #vender(    a(e[0]),     e[1],       a(e[0]).precio_BI())
@@ -111,66 +114,69 @@ def vuelta(a,b,c,d):
                             #vender(    c(e[2]),     comproB,    c(e[2]).precio_BI())
                             #comprar(   d(e[0]),     comproA,    d(e[0]).precio_OF())
 
-                            gana += comproA - e[1]
-                            limite -= e[1]
+                            if e[0] == 'al30' or e[2] == 'al30':
+                                ganaAL30 += comproA - e[1]
+                                limite -= e[1]
+
                             moneda1 += round(saldoA,2)
                                  
                             if len(str(t3)) >= 4:
                                 moneda3 += round(saldoB,2)
-                                print(f'{e[0]}/{e[2]}, bonos:{gana} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
+                                print(f'{e[0]} /{e[2]}, bonos:{ganaAL30} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
                                 continue
                             else:
                                 moneda2 += round(saldoB,2)
-                                print(f'{e[0]}/{e[2]}, bonos:{gana} / u$d{moneda1} / u$s{moneda2} / Ars{moneda3} _ lim:{limite}')
+                                print(f'{e[0]}/{e[2]}, bonos:{ganaAL30} / u$d{moneda1} / u$s{moneda2} / Ars{moneda3} _ lim:{limite}')
                                 continue
                         else:
-                            print(f' Sin bid/ask {e[0]}/{e[2]} bonos:{gana} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
+                            print(f' Sin bid/ask {e[0]} /{e[2]} bonos:{ganaAL30} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
                             break
                     else:
-                        print(f'//NO con {e[0]}/{e[2]} bonos:{gana} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
+                        print(f'//NO con {e[0]} /{e[2]} bonos:{ganaAL30} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
                         break
                 else:
-                    print(f' Sin precios en {e[0]}/{e[2]} bonos:{gana} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
+                    print(f' Sin precios en {e[0]} /{e[2]} bonos:{ganaAL30} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3} _ lim:{limite}')
                     break
             else:
-                print(f' Limite {limite} AGOTADO !!! bonos:{gana} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3}')
+                print(f' Limite {limite} AGOTADO !!! bonos:{ganaAL30} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3}')
                 break
 
 
 costos = 0.0026
 limite = 100000
-gana = round(0,0)
+ganaAL30 = round(0,0)
 moneda1 = round(0,2)
 moneda2 = round(0,2)
 moneda3 = round(0,2)
 
 ins = {
-    '1':['al30',100,'gd30'],'2':['al30',100,'gd35'],'3':['al30',100,'gd38'],'4':['al30',100,'s31e2'],'5':['al30',100,'s28f2'],'6':['al30',1000,'aapl'],'6':['aapl',10,'al30'], 
-    '10':['gd30',100,'al30'],'11':['gd30',100,'gd35'],'12':['gd30',100,'gd38'],'13':['gd30',100,'s31e2'],'14':['gd30',100,'s28f2'],'15':['gd30',1000,'aapl'],'16':['aapl',10,'gd30']
+    '1':['al30',100,'gd30'],'2':['al30',100,'gd35'],'3':['al30',100,'gd38'],'4':['al30',100,'s31e2'],'5':['al30',100,'s28f2'],'6':['al30',500,'aapl'],'6':['aapl',1,'al30'], 
+    '10':['gd30',100,'al30'],'11':['gd30',100,'gd35'],'12':['gd30',100,'gd38'],'13':['gd30',100,'s31e2'],'14':['gd30',100,'s28f2'],'15':['gd30',500,'aapl'],'16':['aapl',1,'gd30']
     }
 
-cant = ins['1'][1]
 
 while True:
     if time.strftime("%H:%M:%S") <= '11:00:05':
         print('Esperando apertura del mercado ... ',time.strftime("%H:%M:%S")),time.sleep(1)
         continue
     elif time.strftime("%H:%M:%S") >= '16:59:50':
-        print(f'... MERCADO CERRADO 17HS ... Limite {limite} bonos ganados:{gana} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3}')
+        print(f'... MERCADO CERRADO 17HS ... Limite {limite} bonos ganados:{ganaAL30} / u$d{moneda1} / u$d{moneda2} / Ars{moneda3}')
         break
     if time.strftime("%H:%M:%S") <= '15:59:15' and limite > 0:
         vuelta(cclCI,cclCI,mepCI,mepCI)
         vuelta(mepCI,mepCI,cclCI,cclCI)
         vuelta(cclCI,cclCI,pesCI,pesCI)
         vuelta(mepCI,mepCI,pesCI,pesCI)
-    else: break
+    #else: break
 
     if limite > 0:
         vuelta(ccl48,ccl48,mep48,mep48)
         vuelta(mep48,mep48,ccl48,ccl48)
         vuelta(ccl48,ccl48,pes48,pes48)
         vuelta(mep48,mep48,pes48,pes48)
+    else: break
 
+    if limite > 0:
         vuelta(ccl24,ccl24,mep24,mep24)
         vuelta(mep24,mep24,ccl24,ccl24)
         vuelta(ccl24,ccl24,pes24,pes24)
