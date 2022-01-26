7ab6c508d99c9168efc347707f8d8b481f2b6a32
Francisco <piolifl@gmail.com>
2022-01-26 20:27:15 -0300

    Reajustado precios y cantidades

--- bonos_v1.py
+++ bonos_v1.py
@@ -8,7 +8,7 @@ pr = Consultar()
 op = Operar()
 
 costos = 0.0026
-limite = 1000
+limite = 300
 gAL30 = 0
 gGD30 = 0
 gAAPL = 0
@@ -19,14 +19,15 @@ pesos = round(0,2)
 mep = round(0,2)
 ccl = round(0,2)
 
-moneda = {'ccl-mep':['C','C','D','D'],  'mep-ccl':['D','D','C','C'],  'mep-pes':['D','D','',''] , 'ccl-pes':['C','C','','']}
+moneda = {'ccl-mep':['C','D'],  'mep-ccl':['D','C'],  'mep-pes':['D',''] , 'ccl-pes':['C','']}
 
 plazo = ['CI','48hs','24hs']
 
-par = { '1':['al30',200,'gd30'], '2':['al30',200,'s31e2'], '3':['al30',200,'aapl'],
-        '10':['gd30',200,'al30'],'11':['gd30',200,'s31e2'],'12':['gd30',200,'aapl'],
-        '20':['s31e2',8000,'al30'],'21':['s31e2',8000,'gd30'],'22':['s31e2',8000,'aapl'],
-        '30':['aapl',5,'al30'],'31':['aapl',5,'gd30'],'30':['aapl',5,'s31e2']           }
+par = { '1':['al30',10,'gd30'], '2':['al30',10,'s31e2'],# '3':['al30',200,'aapl'],
+        '10':['gd30',10,'al30'], '11':['gd30',10,'s31e2'],# '12':['gd30',200,'aapl'],
+        '20':['s31e2',8000,'al30'], '21':['s31e2',8000,'gd30'],# '22':['s31e2',8000,'aapl'],
+        '30':['aapl',5,'al30'], '31':['aapl',5,'gd30'], '30':['aapl',5,'s31e2']           
+        }
 
 def ganaBonos(bono:str):
     global gAL30,gGD30,gAAPL,gS31E2
@@ -38,73 +39,116 @@ def ganaBonos(bono:str):
 def ganaMoneda(moneda:str):
     global pesos,mep,ccl
     if moneda == 'ccl-mep':
-        ccl   += round( vendoA - (comproB * (pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'') / 100) )     ,3)
-        mep   += round( vendoB - (comproA * (pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') / 100) )     ,3)
+        if e[0] == 'aapl' or e[0] == 'ko': 
+            ccl     += round( vendoA - (comproB *  precioB ), 3)
+            mep     += round( vendoB - (comproA *  precioA ), 3)
+        else:                              
+            ccl     += round( vendoA - (comproB * (precioB / 100) ), 3)
+            mep     += round( vendoB - (comproA * (precioA / 100) ), 3)
 
     elif moneda == 'mep-ccl':
-        mep   += round( vendoA - (comproB * (pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'') / 100) )     ,3)
-        ccl   += round( vendoB - (comproA * (pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') / 100) )     ,3)
+        if e[0] == 'aapl' or e[0] == 'ko':
+            mep     += round( vendoA - (comproB *  precioB ), 3)
+            ccl     += round( vendoB - (comproA *  precioA ), 3)
+        else:                              
+            mep     += round( vendoA - (comproB * (precioB / 100) ), 3)
+            ccl     += round( vendoB - (comproA * (precioA / 100) ), 3)
 
     elif moneda == 'ccl-pes':
-        ccl   += round( vendoA - (comproB * (pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'') / 100) )     ,3)
-        pesos += round( vendoB - (comproA * (pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') / 100) )     ,3)
+        if e[0] == 'aapl' or e[0] == 'ko':
+            ccl     += round( vendoA - (comproB *  precioB ), 3)
+            pesos   += round( vendoB - (comproA *  precioA ), 3)
+        else:                              
+            ccl     += round( vendoA - (comproB * (precioB / 100) ), 3)
+            pesos   += round( vendoB - (comproA * (precioA / 100) ), 3)
 
     elif moneda == 'mep-pes':
-        mep   += round( vendoA - (comproB * (pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'') / 100) )     ,3)
-        pesos += round( vendoB - (comproA * (pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') / 100) )     ,3)
+        if e[0] == 'aapl' or e[0] == 'ko':
+            mep     += round( vendoA - (comproB *  precioB ), 3)
+            pesos   += round( vendoB - (comproA *  precioA ), 3)
+        else:                              
+            mep     += round( vendoA - (comproB * (precioB / 100) ), 3)
+            pesos   += round( vendoB - (comproA * (precioA / 100) ), 3)
 
+def cruzar(veA,coB,veB,coA):
+    global comproA, comproB, vendoB, vendoA,    precioA, precioB, precioC, precioD
 
+    precioA = pr.precioBI('MERV - XMEV - ' + veA + u[0] + ' - ' + o +'')
+    if e[0] == 'aapl' or e[0] == 'ko': 
+        vendoA = round( precioA * e[1]  * ( 1 - costos), 2)
+    else:                                 
+        vendoA = round((precioA / 100) * e[1]  * ( 1 - costos), 2)
 
+    precioB = pr.precioOF('MERV - XMEV - ' + coB + u[1] + ' - ' + o +'')
+    if e[2] == 'aapl' or e[2] == 'ko': 
+        comproB = vendoA // round( precioB * ( 1 + costos),2)
+    else:                                 
+        comproB = vendoA // round((precioB/100) * ( 1 + costos),2)
 
-if time.strftime("%H:%M:%S") > '15:59:45':
-    plazo = ['48hs','24hs']
+    precioC = pr.precioBI('MERV - XMEV - ' + veB + u[1] + ' - ' + o +'')
+    if e[2] == 'aapl' or e[2] == 'ko': 
+        vendoB = comproB * round( precioC * ( 1 - costos),2)
+    else:                                 
+        vendoB = comproB * round((precioC/100) * ( 1 - costos),2)
+
+    precioD = pr.precioOF('MERV - XMEV - ' + coA + u[0] + ' - ' + o +'')
+    if e[0] == 'aapl' or e[0] == 'ko': 
+        comproA = vendoB // round( precioD * ( 1 + costos),2)
+        if precioA != 1000 and precioB != 1000 and precioC != 1000 and precioD != 1000: return comproA
+        else: 
+            comproA = 1000
+            return comproA
+    else: 
+        comproA = vendoB // round((precioD / 100) * ( 1 + costos),2)
+        if precioA != 1000 and precioB != 1000 and precioC != 1000 and precioD != 1000: return comproA
+        else:  
+            comproA = 1000
+            return comproA
+
+if time.strftime("%H:%M:%S") > '15:59:45': plazo = ['48hs','24hs']
+
+################################
 
 while True:
     if time.strftime("%H:%M:%S") < '11:00:00':
-        print(time.strftime("%H:%M:%S"),'Esperando la apertura ... '),time.sleep(1)
+        print(time.strftime("%H:%M:%S"),'Esperando la apertura a las 11hs ... '),time.sleep(10)
         continue
-    if limite < 200 or time.strftime("%H:%M:%S") > '22:59:50':
-        print(f'FIN 17HS | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} || ccl {ccl} | mep {mep} | pesos {pesos}')
+    if time.strftime("%H:%M:%S") > '23:59:50':
+        print(f'FIN 17HS CERRADO !!!   | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} || ccl {ccl} | mep {mep} | pesos {pesos}')
         break
+    if limite < 200: 
+        print(f'FIN LIMITE AGOTADO !!! | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} || ccl {ccl} | mep {mep} | pesos {pesos}')
+        break  
+    
     for a,e in par.items():
         for o in plazo:
-            for i,u in moneda.items():
+            for i,u in moneda.items(): 
 
-                if e[0] == 'aapl' or e[0] == 'ko':  vendoA = pr.precioBI('MERV - XMEV - ' + e[0].upper() + u[0] + ' - ' + 0 +'')  * e[1] * ( 1 - costos)
-                else: vendoA = (pr.precioBI('MERV - XMEV - ' + e[0].upper() + u[0] + ' - ' + o +'') / 100)  * e[1] * ( 1 - costos)
-                if vendoA == 1000: continue
+                cruzar(     e[0].upper(),   e[2].upper(),   e[2].upper(),   e[0].upper()    )
 
-                if e[0] == 'aapl' or e[0] == 'ko':  comproB = vendoA // pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + 0 +'') * ( 1 + costos)
-                else: comproB = vendoA // (pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'')/100) * ( 1 + costos)
-                if comproB == 1000: continue
+                if comproA == 1000: 
+                    print(f'Sin precios {e[0].upper()}{u[0]} {o} conta {e[2].upper()}{u[0]} {o} '),time.sleep(1)
+                    break
 
-                if e[0] == 'aapl' or e[0] == 'ko':  vendoB = comproB * pr.precioBI('MERV - XMEV - ' + e[2].upper() + u[2] + ' - ' + o +'') * ( 1 - costos)
-                else: vendoB = comproB * (pr.precioBI('MERV - XMEV - ' + e[2].upper() + u[2] + ' - ' + o +'')/100) * ( 1 - costos)
-                if vendoB == 1000: continue
+                if limite < 200:  break
 
-                if e[0] == 'aapl' or e[0] == 'ko':  comproA = vendoB // pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') * ( 1 + costos)
-                else: comproA = vendoB // (pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'')/100) * ( 1 + costos)
-                if comproA == 1000: continue
+                if comproA > e[1]:
 
-                while True: 
-                    if limite < 200:  break
-                    if comproA > e[1]:
-                        if e[0] == 'al30' or e[2] == 'al30': limite -= int( comproA - e[1])
+                    if e[0] == 'al30' or e[2] == 'al30': limite -= int( e[1] )
 
-                        #op.vender   ( ('MERV - XMEV - ' + e[0].upper() + u[0] + ' - ' + o +''), e[1],       pr.precioBI('MERV - XMEV - ' + e[0].upper() + u[0] + ' - ' + o +'') )
-                        #op.comprar  ( ('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +''), comproB,    pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'') )
-                        #op.vender   ( ('MERV - XMEV - ' + e[2].upper() + u[2] + ' - ' + o +''), comproB,    pr.precioBI('MERV - XMEV - ' + e[2].upper() + u[2] + ' - ' + o +'') )
-                        #op.comprar  ( ('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +''), comproA,    pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') )
+                    #op.vender   ( ('MERV - XMEV - ' + e[0].upper() + u[0] + ' - ' + o +''), e[1],       pr.precioBI('MERV - XMEV - ' + e[0].upper() + u[0] + ' - ' + o +'') )
+                    #op.comprar  ( ('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +''), comproB,    pr.precioOF('MERV - XMEV - ' + e[2].upper() + u[1] + ' - ' + o +'') )
+                    #op.vender   ( ('MERV - XMEV - ' + e[2].upper() + u[2] + ' - ' + o +''), comproB,    pr.precioBI('MERV - XMEV - ' + e[2].upper() + u[2] + ' - ' + o +'') )
+                    #op.comprar  ( ('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +''), comproA,    pr.precioOF('MERV - XMEV - ' + e[0].upper() + u[3] + ' - ' + o +'') )
 
-                        ganaBonos(e[0])
-                        ganaMoneda(i)
+                    ganaBonos(e[0])
+                    ganaMoneda(i)
 
-                        print(f'    SI | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} | ccl {ccl} | mep {mep} | pesos {pesos}')
+                    print(f'    SI | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} | ccl {ccl} | mep {mep} | pesos {pesos}')
                         
-                        pr.log('AL30: ' + str(gAL30) + ' | GD30: ' + str(gGD30) + ' | S31E2: ' + str(gS31E2) + ' | AAPL: ' + str(gAAPL) + ' || MEP: ' + str(mep) + ' | CCL: ' + str(ccl) + ' | PESOS: ' + str(pesos))
+                    pr.log('AL30: ' + str(gAL30) + ' | GD30: ' + str(gGD30) + ' | S31E2: ' + str(gS31E2) + ' | AAPL: ' + str(gAAPL) + ' || MEP: ' + str(mep) + ' | CCL: ' + str(ccl) + ' | PESOS: ' + str(pesos))
                         
-                    else: 
-                        print(time.strftime("%H:%M:%S"),f' NO | {i} | {e[0].upper()} / {e[2].upper()} {o} | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} || ccl {ccl} | mep {mep} | pesos {pesos}')
-                        break
-                    
-    if limite < 200:  break                   
+                else: 
+                    print(time.strftime("%H:%M:%S"),f' NO | {i} | {e[0].upper()} / {e[2].upper()} {o} | limite {limite} | al30 {gAL30} | gd30 {gGD30} | s31e2 {gS31E2} | aapl {gAAPL} || ccl {ccl} | mep {mep} |pesos {pesos}')
+                    break
+                           
