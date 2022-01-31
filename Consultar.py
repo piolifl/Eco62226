c2e33f4ac2046bd018993e041aed830262f388b7
Francisco <piolifl@gmail.com>
2022-01-31 19:00:49 -0300

    Ajuste en bid/offers

--- Consultar.py
+++ Consultar.py
@@ -10,25 +10,34 @@ class Consultar(ECO_62226):
 
     def precioLA(self,ticker=str):
         var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.LAST])
-        try:
-            return var['marketData']['LA']['price']
-        except:
-            return 1000
+        try: return var['marketData']['LA']['price']
+        except: return 1000
 
     def precioBI(self,ticker=str):
         var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.BIDS])
-        try:
-            return var['marketData']['BI'][0]['price']
-        except:
-            return 1000
+        try: return var['marketData']['BI'][0]['price']
+        except: return 1000
 
     def precioOF(self,ticker=str):
         var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.OFFERS])
-        try:
-            return var['marketData']['OF'][0]['price']
-        except:
-            return 1000
+        try: return var['marketData']['OF'][0]['price']
+        except: return 1000
     
+    def bidsBI(self,ticker=str):
+        var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.BIDS])
+        try: return var['marketData']['BI'][0]['size']
+        except: return 0
+
+    def offersOF(self,ticker=str):
+        var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.OFFERS])
+        try: return var['marketData']['OF'][0]['size']
+        except: return 0
+
+    def lastLA(self,ticker=str):
+        var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.LAST])
+        try: return var['marketData']['LA'][0]['size']
+        except: return 0
+
     def logRulos(self, texto:str):
         f = open('rulos.log','a')
         f.write( datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' | ' + texto + '\n' )
@@ -39,30 +48,40 @@ class Consultar(ECO_62226):
         f.write( datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' | ' + texto + '\n' )
         f.close()
         
+
 class Consultar2(BCCH_2474):
     def __init__(self):
         BCCH_2474.__init__(self)
 
     def precioLA(self,ticker=str):
         var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.LAST])
-        try:
-            return var['marketData']['LA']['price']
-        except:
-            return 1000
+        try: return var['marketData']['LA']['price']
+        except: return 1000
 
     def precioBI(self,ticker=str):
         var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.BIDS])
-        try:
-            return var['marketData']['BI'][0]['price']
-        except:
-            return 1000
+        try: return var['marketData']['BI'][0]['price']
+        except: return 1000
 
     def precioOF(self,ticker=str):
         var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.OFFERS])
-        try:
-            return var['marketData']['OF'][0]['price']
-        except:
-            return 1000
+        try: return var['marketData']['OF'][0]['price']
+        except: return 1000
+    
+    def bidsBI(self,ticker=str):
+        var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.BIDS])
+        try: return var['marketData']['BI'][0]['size']
+        except: return 0.0
+
+    def offersOF(self,ticker=str):
+        var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.OFFERS])
+        try: return var['marketData']['OF'][0]['size']
+        except: return 0.0
+
+    def lastLA(self,ticker=str):
+        var = pyRofex.get_market_data(ticker,entries=[pyRofex.MarketDataEntry.LAST])
+        try: return var['marketData']['LA'][0]['size']
+        except: return 0.0
     
     def logRulos(self, texto:str):
         f = open('rulos.log','a')
@@ -73,3 +92,4 @@ class Consultar2(BCCH_2474):
         f = open('ratios.log','a')
         f.write( datetime.now().strftime("%Y-%m-%d %H:%M:%S") + ' | ' + texto + '\n' )
         f.close()
+
--- opciones.py
+++ opciones.py
@@ -16,7 +16,7 @@ ratio = {
 
 while True:
     if time.strftime("%H:%M:%S") < '11:00:00':
-        print(time.strftime("%H:%M:%S"),'Esperando la apertura a las 11hs ... '),time.sleep(10)
+        print(time.strftime("%H:%M:%S"),'Esperando la apertura a las 11hs ... '),time.sleep(5)
         continue
     if time.strftime("%H:%M:%S") > '16:59:50':
         print(f'FIN 17hs CERRADO |')
@@ -25,21 +25,24 @@ while True:
         if valor[2] == '0': continue
         vendo  = pr.precioBI('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs')
         compro = pr.precioOF('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs')
-        if vendo == 1000 or compro == 1000:
-            print(time.strftime("%H:%M:%S"),f' | SIN PRECIOS | {valor[0]}{valor[1]}{valor[2]}{valor[3]} a {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]} a {compro} '),time.sleep(1)
-            continue
+        if vendo == 1000 or compro == 1000: continue
         ratioE = round(valor[5] / valor[8],3) 
         ratioA = round(vendo / compro,2)
         res = round(    (((valor[4] * valor[5])*(1+costo)) - ((vendo * valor[4])*(1-costo)))    -   (   ((valor[7] * valor[8])*(1-costo)) - ((compro * valor[7]) * (1+costo) ) )   ,2)
         if  ratioA > ratioE * (1 + 0.25):
 
+            if   pr.bidsBI(  'MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs') == 0: continue
+            elif pr.offersOF('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs') == 0: continue
+
             #op.vender   (('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs'),  valor[4],   vendo)
             #op.comprar  (('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs'),  valor[7],   compro)
 
-            pr.logRatios('CERRADO: ' + str(valor[2]) + ' a ' + str(vendo) + ' x ' + str(valor[4]) + ' contra ' + str(valor[6]) + ' a ' + str(compro) + ' x ' + str(valor[7]) + ' resultado ' + str(res))
             pr.logRatios('CERRADO: ' + str(valor[0],valor[1],valor[2],valor[3]) + ' | '  + str(valor[0],valor[1],valor[6],valor[3]) + ' | ' + ' Resultado ' + str(res)   )
 
             print(time.strftime("%H:%M:%S"),f' | CERRADO | {valor[0]}{valor[1]}{valor[2]}{valor[3]} a {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]} a {compro} resultado {res}')
             ratio[item][2] = '0'
 
         else: print(time.strftime("%H:%M:%S"),f' | BUSCANDO | {valor[0]}{valor[1]}{valor[2]}{valor[3]} a {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]} a {compro} resultado {res}')
+        time.sleep(5)
+
+        
