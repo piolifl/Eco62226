896589d0c5c3b38a95f9d1c1488b8b47bc97e8bf
Francisco <piolifl@gmail.com>
2022-02-17 16:27:19 -0300

    Opciones final

--- opciones.py
+++ opciones.py
@@ -28,19 +28,19 @@ while True:
         if vendo == 1000 or compro == 1000: continue
         ratioE = round(valor[5] / valor[8],3) 
         ratioA = round(vendo / compro,2)
-        res = round(    ((((valor[4] * valor[5])*(1+costo)) - ((vendo * valor[4])*(1-costo)))    -   (   ((valor[7] * valor[8])*(1-costo)) - ((compro * valor[7]) * (1+costo) ) )) *100  ,2)
+        res = round(    ((((valor[7] * valor[8])*(1-costo)) - ((compro * valor[7]) * (1+costo) ) ) - (((valor[4] * valor[5])*(1+costo)) - ((vendo * valor[4])*(1-costo)))    ) *100  ,2)
 
-        if  ratioA > ratioE * (1 + 0.5):
+        if  ratioA > (ratioE * (1 + 0.5)):
 
             if   pr.bidsBI(  'MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs') == 0: continue
             elif pr.offersOF('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs') == 0: continue
 
-            #op.vender   (('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs'),  valor[4],   vendo)
-            #op.comprar  (('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs'),  valor[7],   compro)
+            #op.vender   (('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs'),  valor[4],   round(vendo,3))
+            #op.comprar  (('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs'),  valor[7],   round(compro,3))
 
             pr.logRatios('CERRADO: ' + str(valor[0]) + ' | '  + str(valor[0]) + ' | ' + ' Resultado ' + str(res)   )
 
-            print(time.strftime("%H:%M:%S"),f' | CERRADO | {valor[0]}{valor[1]}{valor[2]}{valor[3]}: {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]}: {compro} | ratioE {ratioE} ratioA {ratioA} | resultado: {res}')
+            print(time.strftime("%H:%M:%S"),f'| CERRADO | {valor[0]}{valor[1]}{valor[2]}{valor[3]}: {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]}: {compro} | ratioE {ratioE} ratioA {ratioA} | resultado: {res}')
 
             ratio[item][2] = '0'
 
