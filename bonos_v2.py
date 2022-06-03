69961a154dcc757ae26101a4511cf7d2f7f24019
Francisco <piolifl@gmail.com>
2022-06-03 11:21:38 -0300

    Bonos ok

--- bonos_v2.py
+++ bonos_v2.py
@@ -14,6 +14,7 @@ gana = 0
 ccl = 0
 mep = 0
 pes = 0
+bonos = {'al30':0,'gd30':0,'gd35':0}
 
 tipo = {'al':['30'],'gd':['35','30'],
 #'al':['30','29','35','41'],'ae':['38'],'gd':['30','29','35','38','41','46'],'aap':['L'],'k':['O'],'amz':['N']
@@ -127,32 +128,34 @@ while True:
                                     elif comproB[2] <= comB: break
                                     elif vendoB[1] <= comB:  break
                                     elif comproA[2] <= comA:  break
-                                    #bid/off
+                                    #PUNTAS: bid/off
                                     '''if vendoA[1] <= uso: break
                                     elif comproB[1] <= comB: break
-                                    elif vendoB[1] <= comB:  break
-                                    elif comproA[1] <= comA:  break'''
+                                    elif vendoB[1] <= comB: break
+                                    elif comproA[1] <= comA: break'''
 
                                     #op.vender   ( ( 'MERV - XMEV - ' + clave.upper() + a + i[0] + ' - ' + u ) , uso , vendoA[0] )
                                     #op.comprar  ( ( 'MERV - XMEV - ' + cla.upper() + aa + i[0] + ' - ' + u)   , comB, comproB[0] )
                                     #op.vender   ( ( 'MERV - XMEV - ' + cla.upper() + aa + i[1] + ' - ' + u )  , comB, vendoB[0] )
                                     #op.comprar  ( ( 'MERV - XMEV - ' + clave.upper() + a + i[1] + ' - ' + u ) , comA, comproA[0] )
 
+                                    for bonos_clave, bonos_valor in bonos.items():
+                                        if bonos_clave == clave+a: bonos_clave += res
+
                                     print(' SI ' + clave.upper() + a + i[0] + '-' + u + ' '+ str(vendoA[0])+'|', end='')
                                     print(cla.upper() + aa + i[0] + '-' + u + ' ' + str(comproB[0] )+ '|', end='')
                                     print(cla.upper() + aa + i[1] + '-' + u + ' ' + str(vendoB[0])+ '|', end='')
-                                    print(clave.upper() + a + i[1] + '-' + u + ' '+ str(comproA[0])+'|lim:'+ str(limite)+'|RES:'+str(round(res,2))+'|' +str(gana)+'|ccl:'+str(round(ccl,2))+'|usd:'+str(round(mep,2))+'|ars:'+str(round(pes,2)))
+                                    print(clave.upper() + a + i[1] + '-' + u + ' '+ str(comproA[0])+'|lim:'+ str(limite)+'|RES:'+str(round(res,2))+'|' +str(gana)+'|ccl:'+str(round(ccl,2))+'|usd:'+str(round(mep,2))+'|ars:'+str(round(pes,2))+bonos)
 
                                     resultado(e,clave,cla)
                                     if (clave + a) == 'al30' or (cla + aa) == 'al30': limite -= nominal[0]
                                     gana += res
                                     continue
-
                                 else:
                                     print('|'+clave.upper()+a+i[0]+u.lower()+''+str(vendoA[0])+'x'+str(uso)+'=$'+str(venA)+'|',end='')
                                     print(cla.upper()+aa+i[0]+u.lower()+''+str(comproB[0])+'/$'+str(venA)+'='+str(comB)+'|',end='') 
                                     print(cla.upper()+aa+i[1]+u.lower()+''+str(vendoB[0])+'x'+str(comB)+'=$'+str(round(venB*(comB/100),2) )+'|', end='') 
-                                    print(clave.upper()+a+i[1]+u.lower()+' '+str(comA)+'|'+str(limite)+'|'+str(round(gana,2))+'|'+str(round(ccl,2))+'|'+str(round(mep,2))+'|'+ str(round(pes,2)))
+                                    print(clave.upper()+a+i[1]+u.lower()+' '+str(comA)+'|'+str(limite)+'|'+str(round(gana,2))+'|'+str(round(ccl,2))+'|'+str(round(mep,2))+'|'+ str(round(pes,2))+bonos)
 
                                     break
 
---
+++ tempo.py
@@ -0,0 +1,22 @@
+
+bonos = {}
+
+while True:
+    a=input('continua ...')
+    if a != '':
+        print(bonos)
+        break
+    clave = input('bono: ')
+    valor = input('valor: ')
+    bonos[clave]= valor
+    print(bonos)
+    
+
+
+
+
+
+
+
+
+
