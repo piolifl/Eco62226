203822a6a78e3a2016c443f91f3863d3fed4f8bc
Francisco <piolifl@gmail.com>
2022-06-16 15:12:17 -0300

    Opciones: cargo ratio

--- opciones.py
+++ opciones.py
@@ -1,3 +1,4 @@
+from http.client import CONTINUE
 from Consultar import Consultar
 from Operar import Operar
 import math
@@ -6,13 +7,12 @@ import time
 pr = Consultar()
 op = Operar()
 
-costo = 0.005
+costo = 0.0052
 
 ratio = {
-    '1':['GFG','C','210','.JU',1,22.5,  '230',2,   12.3,  0.15 ],
+    '1':['GFG','C','185','.AG',1,10.88,  '195',2,   6.5,  0.15 ],
     '2':['GFG','C','0','.JU',1,24.93, '220',2,   14,    0.15 ],
-    '3':['GFG','C','0','.AB',1,20.3,   '220',2,   11.15,  0.15 ],
-    '4':['GFG','C','0','.AB',1,15.95,  '230',2,   8.61,   0.15 ]
+    '3':['GFG','C','0','.AB',1,20.3,   '220',2,   11.15,  0.15 ]
 }
 
 while True:
@@ -24,31 +24,28 @@ while True:
         break
     for item, valor in ratio.items():
         if valor[2] == '0': continue
-        vendo  = pr.precioBI('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs')
-        compro = pr.precioOF('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs')
-        if vendo == 1000 or compro == 1000: continue
+        vendo = pr.precioBI('MERV - XMEV - '+valor[0]+valor[1]+valor[2]+valor[3]+' - '+'24hs')
+        compro= pr.precioOF('MERV - XMEV - '+valor[0]+valor[1]+valor[6]+valor[3]+' - '+'24hs')
+        if vendo[0] == 10000 or compro[0] == 10000: continue
 
         ratioE = round(valor[5] / valor[8],3) 
-        ratioA = round(vendo / compro,2)
-        res = round(((((valor[7] * valor[8])*(1-costo)) - ((compro * valor[7]) * (1+costo) ) ) - (((valor[4] * valor[5])*(1+costo)) - ((vendo * valor[4])*(1-costo)))) *100,2)
+        ratioA = round(vendo[0] / compro[0],3)
+        ratioO = round(ratioE * (1+ valor[9]),3)
+
+        res = round(((((valor[7] * valor[8])*(1-costo)) - ((compro[0] * valor[7]) * (1+costo) ) ) - (((valor[4] * valor[5])*(1+costo)) - ((vendo[0] * valor[4])*(1-costo)))) *100,2)
         dif = round((ratioA/ratioE -1)*100,2)
 
-        if  ratioA > (ratioE * (1 + valor[9])):
+        if  ratioA > ratioO:
+            if vendo[1] == 0 or compro[1] == 0: continue
+            
+            op.comprar  (('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs'),  valor[7],   round(compro[0],3))
+            op.vender   (('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs'),  valor[4],   round(vendo[0],3))
 
-            if   pr.bidsBI(  'MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs') == 0: continue
-            elif pr.offersOF('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs') == 0: continue
-
-            #git SIN gatillo - ratios actuales
-            #op.vender   (('MERV - XMEV - ' + valor[0] + valor[1] + valor[2] + valor[3] +' - ' + '24hs'),  valor[4],   round(vendo,3))
-            #op.comprar  (('MERV - XMEV - ' + valor[0] + valor[1] + valor[6] + valor[3] +' - ' + '24hs'),  valor[7],   round(compro,3))
-
-            pr.logRatios('CERRADO: ' + str(valor[0]) + ' | '  + str(valor[0]) + ' | ' + ' Resultado ' + str(res)   )
-
-            print(time.strftime("%H:%M:%S"),f'| CERRADO | {valor[0]}{valor[1]}{valor[2]}{valor[3]}: {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]}: {compro} || resultado: {res}')
+            print(time.strftime("%H:%M:%S"),f'| CERRADO | {valor[0]}{valor[1]}{valor[2]}{valor[3]}: {vendo[0]} | {valor[0]}{valor[1]}{valor[6]}{valor[3]}: {compro[0]} || re: {res} raE: {ratioE} raA: {ratioA} raO: {ratioO} dif: {dif}')
 
             ratio[item][2] = '0'
 
-        else: print(time.strftime("%H:%M:%S"),f'| {item} | {valor[0]}{valor[1]}{valor[2]}{valor[3]}: {vendo} | {valor[0]}{valor[1]}{valor[6]}{valor[3]}: {compro} || resultado: {res}')
+        else: print(time.strftime("%H:%M:%S"),f'| {item} | {valor[0]}{valor[1]}{valor[2]}{valor[3]}: {vendo[0]} | {valor[0]}{valor[1]}{valor[6]}{valor[3]}: {compro[0]} || re: {res} raE: {ratioE} raA: {ratioA} raO: {ratioO} dif: {dif}')
         time.sleep(2)
             
 
